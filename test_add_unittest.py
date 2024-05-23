77ae137898203d4bd9c4a867f87756ee37873d3e
varjang <varjangnandaniya8@gmail.com>
2024-05-23 09:09:06 +0530

    test3: add test for testing adding multiple digits string

--- test_add_unittest.py
+++ test_add_unittest.py
@@ -10,6 +10,9 @@ class TestAdd(unittest.TestCase):
     def test_add_single_digit(self):
         self.assertEqual(add_mod.add("1"), 1)
 
+    def test_add_multiple_digits(self):
+        self.assertEqual(add_mod.add("1,5"), 6)
+
 
 if __name__ == "__main__":
     unittest.main()
